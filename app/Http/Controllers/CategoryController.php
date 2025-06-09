5f0c155d26c6256f8ec7a187e0a2be0196843254
Ashif <contact@ashifsadiq.in>
2025-06-09 11:27:52 +0530

    feat: Implement category listing and product retrieval by category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,10 +1,21 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return Category::paginate();
+    }
+    public function show(Request $request, $slug)
+    {
+        $category = Category::where("slug", $slug)->firstOrFail();
+        $products = Product::where("category_id", $category->id);
+        return ProductResource::collection($products->paginate(10));
+    }
 }
