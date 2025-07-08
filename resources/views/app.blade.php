a23f819986853800cea503a22e9bf9e7e2cd120f
Ashif Sadiq <ashifsadiq@gmail.com>
2025-07-08 14:30:58 +0530

    adding enhancement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -58,7 +58,7 @@
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
-        @if(count($schemas))
+        @if($schemas??false && count($schemas))
             @foreach($schemas as $schema)
                 <script type="application/ld+json">
                     {!! json_encode($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) !!}
